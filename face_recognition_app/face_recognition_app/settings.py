316d517510788cc8a1e6b0ae97573dd8fa55a04e
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2024-12-05 21:30:18 -0500

    conexion postgres

--- face_recognition_app/face_recognition_app/settings.py
+++ face_recognition_app/face_recognition_app/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'face_recognition_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'face_recognition_app_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Cnic2024#kiki',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
