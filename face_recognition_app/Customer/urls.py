5bf84ac80218564ba6ec7542e16a1e6c663cb439
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2024-12-06 09:01:06 -0500

    logout

--- face_recognition_app/Customer/urls.py
+++ face_recognition_app/Customer/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login, name="login"),
+    path('logout1/', views.logout1, name="logout1"),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
--- face_recognition_app/Customer/views.py
+++ face_recognition_app/Customer/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 # Create your views here.
 @login_required
 def login(request):
+    return redirect('menu')
+
+def logout1(request):
+    logout(request)
     return redirect('menu')
