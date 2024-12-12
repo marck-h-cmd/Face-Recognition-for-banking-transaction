98d86c63e87604d85179562015c268aac8ef39c1
Mac <margeo124@gmail.com>
2024-12-12 18:23:05 -0500

    fix: dotenv directory

--- face_recognition_app/face_recognition_app/utils.py
+++ face_recognition_app/face_recognition_app/utils.py
@@ -4,7 +4,7 @@ from dotenv import dotenv_values
 
 ENV_FILE = Path(__file__).resolve().parent / '.env'
 
+env_vars = dotenv_values(ENV_FILE)
 def get_env_variable(key, default=None):
 
-    env_vars = dotenv_values(ENV_FILE)
     return env_vars.get(key, default)
