d368f2726535ec80e877915fd468a60192353dd0
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2024-12-13 17:20:08 -0500

    m

--- face_recognition_app/face_recognition_app/settings.py
+++ face_recognition_app/face_recognition_app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from ..utils import get_env_variable
+from utils import get_env_variable
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
