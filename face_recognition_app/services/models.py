564f91398eff1c39a6c90ad67bab71d6c3e28650
Mac <margeo124@gmail.com>
2024-12-13 17:35:55 -0500

    Reapply "Merge branch 'master' of https://github.com/marck-h-cmd/Face-Recognition-for-banking-transaction"
    This reverts commit 07c3b324db35f6fac9814eb4e129630f796b7cea.

--- face_recognition_app/services/models.py
+++ face_recognition_app/services/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from banking.models import Account
-# Create your models here.
+
 class Card(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='cards')  
     card_number = models.CharField(max_length=16, unique=True)
-    card_type = models.CharField(max_length=20, choices=[('debit', 'Debit','Debito'), ('credit', 'Credit','Credito')])
+    card_type = models.CharField(
+        max_length=20, 
+        choices=[('debit', 'Debit'), ('credit', 'Credit')], 
+    )
     expiration_date = models.DateField()
-    
-    
